acfc06ef8008bf2c2681a1cb87a395645923c81e
ElAshus <alisagustin5@gmail.com>
2022-01-16 03:06:23 -0300

    third commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Hola';
 const nuevoNum = '5';
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 'True';
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
